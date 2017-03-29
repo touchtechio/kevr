221fce739af93a1158ae8c455e633d88ebe2b801
matt pinner <mpinner@gmail.com>
2017-03-29 13:48:09 -0400

    drone tuning,  headsup display, and  Realsense placement

--- Assets/Scripts/GameControllerVR.cs
+++ Assets/Scripts/GameControllerVR.cs
@@ -76,11 +76,11 @@ public class GameControllerVR : MonoBehaviour
         Debug.Log("displays connected: " + Display.displays.Length);
         // Display.displays[0] is the primary, default display and is always ON.
         // Check if additional displays are available and activate each.
-        if (Display.displays.Length > 1)
+      //  if (Display.displays.Length > 1)
             Display.displays[1].Activate();
-        if (Display.displays.Length > 2)
+      //  if (Display.displays.Length > 2)
             Display.displays[2].Activate();
-        if (Display.displays.Length > 3)
+       // if (Display.displays.Length > 3)
             Display.displays[3].Activate();
 
     }
