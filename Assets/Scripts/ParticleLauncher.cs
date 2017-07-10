1ee7e6e791eb552dd5f80b65d17b11a34217b3d0
adellelin <adelle.lin@gmail.com>
2017-07-09 17:58:02 -0700

    wired up particle launcher on both gloves

--- Assets/Scripts/ParticleLauncher.cs
+++ Assets/Scripts/ParticleLauncher.cs
@@ -16,17 +16,17 @@ public class ParticleLauncher : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        /*
+        
 
         if (Input.GetButton("Fire1"))
 
         {
 
-            ParticleSystem.MainModule psMain = particleLauncher.main; // calling variables from the main module
+            ParticleSystem.MainModule psMain = particleLauncher[0].main; // calling variables from the main module
             // psMain.startColor = particleColorGradient.Evaluate(UnityEngine.Random.Range(0f, 1f));
-            particleLauncher.Emit(1);
+            particleLauncher[0].Emit(1);
         }
-        */
+        
 
     }
 
