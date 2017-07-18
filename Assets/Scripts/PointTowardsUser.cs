ebd17777c383a7e633ed812bbd1eacec0a759766
milan cengija <milanx.cengija@intel.com>
2017-07-17 18:19:50 -0700

    point towards now finds headset imu gloves updated

--- Assets/Scripts/PointTowardsUser.cs
+++ Assets/Scripts/PointTowardsUser.cs
@@ -4,14 +4,21 @@ using UnityEngine;
 
 public class PointTowardsUser : MonoBehaviour {
 
-    public Transform target;
-    public Transform source;
+  //  public Transform target;
+    private Transform targetObject;
+  //  public Transform source;
     public float speed;
+
+    void Start()
+    {
+        targetObject = GameObject.Find("[CameraRig]").transform.GetChild(2);
+        
+    }
 
     void Update()
     {
         //float step = speed * Time.deltaTime;
-        transform.position = Vector3.MoveTowards(source.position, target.position, 0.0f);
+        transform.position = Vector3.MoveTowards(transform.position, targetObject.position, 0.0f);
         Orient();
     }
 
@@ -19,7 +26,7 @@ public class PointTowardsUser : MonoBehaviour {
     void Orient()
     {
         //transform.LookAt(target.position);
-       transform.LookAt(new Vector3(target.position.x, transform.position.y, target.position.z));
+       transform.LookAt(new Vector3(targetObject.position.x, transform.position.y, targetObject.position.z));
         Vector3 rot = transform.rotation.eulerAngles;
         rot = new Vector3(rot.x, rot.y + 180, rot.z);
         transform.rotation = Quaternion.Euler(rot);
